730722a6b94906b6cc3190ef16d02322de95301c
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2023-02-23 20:30:59 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import classNames from 'classnames';
+import './App.scss';
 
-function App() {
+import usersFromServer from './api/users';
+import productsFromServer from './api/products';
+import categoriesFromServer from './api/categories';
+
+import { User } from './types/User';
+import { Category } from './types/Category';
+import { Product } from './types/Product';
+
+import { ProductTable } from './components/ProductTable/ProductTable';
+import { Categories } from './components/Category/Categories';
+import { Users } from './components/User/Users';
+import { Query } from './components/Query/Query';
+
+const productMerged: Product[] = productsFromServer.map(product => {
+  const categoryFromServer: Category | undefined = categoriesFromServer.find(
+    category => category.id === product.categoryId,
+  );
+
+  const userFromServer: User | undefined = usersFromServer.find(
+    user => user.id === categoryFromServer?.ownerId,
+  );
+
+  return {
+    ...product,
+    category: categoryFromServer,
+    user: userFromServer,
+  };
+});
+
+export const App: React.FC = () => {
+  const products: Product[] = productMerged;
+  const [filterUser, setFilterUser] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [query, setQuery] = useState('');
+
+  let visibleProducts = products.filter(product => {
+    if (filterUser === '') {
+      return product;
+    }
+
+    return product.user?.name === filterUser;
+  });
+
+  visibleProducts = visibleProducts.filter(product => {
+    return product.name.toLowerCase().includes(query.toLowerCase());
+  });
+
+  visibleProducts = visibleProducts.filter(product => {
+    if (selectedCategory === '') {
+      return product;
+    }
+
+    return product.category?.title === selectedCategory;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Product Categories</h1>
+
+        <div className="block">
+          <nav className="panel">
+            <p className="panel-heading">Filters</p>
+
+            <p className="panel-tabs has-text-weight-bold">
+              <Users
+                usersFromServer={usersFromServer}
+                filterUser={filterUser}
+                setFilterUser={setFilterUser}
+              />
+            </p>
+
+            <div className="panel-block">
+              <Query query={query} setQuery={setQuery} />
+            </div>
+
+            <div className="panel-block is-flex-wrap-wrap">
+              <Categories
+                categoriesFromServer={categoriesFromServer}
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+              />
+            </div>
+
+            <div className="panel-block">
+              <a
+                data-cy="ResetAllButton"
+                href="#/"
+                className="button is-link is-outlined is-fullwidth"
+                onClick={() => {
+                  setFilterUser('');
+                  setSelectedCategory('');
+                  setQuery('');
+                }}
+              >
+                Reset all filters
+              </a>
+            </div>
+          </nav>
+        </div>
+
+        <div className="box table-container">
+          {visibleProducts.length === 0 ? (
+            <p data-cy="NoMatchingMessage">
+              No products matching selected criteria
+            </p>
+          ) : (
+            <ProductTable visibleProducts={visibleProducts} />
+          )}
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
