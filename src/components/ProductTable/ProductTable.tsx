f7268613638b2b686598e92daddab3c30299d24c
Anatolii Voloshyn <a.voloshin.p@gmail.com>
2023-02-23 21:16:27 +0200

    Second commit

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -20,51 +20,24 @@ export const ProductTable: React.FC<Props> = (props) => {
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               ID
-
-              <a href="#/">
-                <span className="icon">
-                  <i data-cy="SortIcon" className="fas fa-sort" />
-                </span>
-              </a>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Product
-
-              <a href="#/">
-                <span className="icon">
-                  <i data-cy="SortIcon" className="fas fa-sort-down" />
-                </span>
-              </a>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               Category
-
-              <a href="#/">
-                <span className="icon">
-                  <i data-cy="SortIcon" className="fas fa-sort-up" />
-                </span>
-              </a>
             </span>
           </th>
 
           <th>
             <span className="is-flex is-flex-wrap-nowrap">
               User
-
-              <a href="#/">
-                <span className="icon">
-                  <i
-                    data-cy="SortIcon"
-                    className="fas fa-sort"
-                  />
-                </span>
-              </a>
             </span>
           </th>
         </tr>
